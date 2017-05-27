588da17bb89f68f0617cab2e5f0ea8f70f88a3ab
Mario Kunz <mario99.kunz@gmail.com>
2017-05-28 01:05:34 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: path.join(__dirname, 'src/js'),
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};
