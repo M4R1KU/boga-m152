c3a339871a229be25005632db705bfe5664ec685
Mario Kunz <mario99.kunz@gmail.com>
2017-05-28 22:47:17 +0200

    include html and css in build

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,61 @@
-var path = require('path');
+let path = require('path');
+let webpack = require('webpack');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/app.js',
+    entry: ['./src/js/app.js', './src/scss/styles.scss'],
     output: {
-        path: __dirname,
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist')
     },
     module: {
         loaders: [
             {
                 test: path.join(__dirname, 'src/js'),
-                loader: 'babel-loader'
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: 'css-loader?importLoaders=1',
+                }),
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'assets/',
+                            publicPath: 'assets/'
+                        }
+                    }
+                ]
             }
         ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+           $: 'jquery'
+        }),
+        new ExtractTextPlugin('styles.bundle.css'),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ],
+    devServer: {
+
     }
 };
